44745dc9db2bc217e9b2a2ae4321eed460d09cfe
Victor Marechal <vmarechal@adwanted.com>
2017-12-28 18:19:01 +0100

    :construction: work in progress

--- server.py
+++ server.py
@@ -6,7 +6,6 @@ from functools import wraps
 import yaml
 import hashlib
 import click
-import json
 
 # CONFIG FILE
 with open("config.yml", 'r') as ymlfile:
@@ -56,4 +55,4 @@ def home():
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True, ssl_context=('cert.pem', 'key.pem'))
--- static/js/main.js
+++ static/js/main.js
@@ -1,26 +1,38 @@
+var successbox =  `
+  <div v-html class="alert alert-danger alert-dismissible fade show" role="alert">
+  <i class="fa fa-exclamation-circle" aria-hidden="true"></i>&nbsp;<b>Authentification OK</b>\nYou will be redirect to your dashboard
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+  <span aria-hidden="true">&times;</span>
+  </button>
+  </div>`
+
+
+
 var app = new Vue({
   delimiters: ['[[', ']]'],
   el: '#loginForm',
   data: {
 	password: '',
 	message: '',
+	success: '<h1 v-html>success!!</h1>',
+	fail: '<h1 v-html>fail!!</h1>',
   },
   methods: {
 	hashPasswd: function() {
 	  var hashPass = CryptoJS.SHA256(this.password).toString();
-	  this.$http.post('http://localhost:5000/login',
+	  this.$http.post($SCRIPT_ROOT+'login',
 		{password: hashPass},
 		{emulateJSON:true},
 		{headers : {'Content-Type': 'application/json'}}
 	  ).then(response => {
 		var resp = response.body.msg
 		if(resp == 'success') {
-		  this.message = 'Authentification OK.\nYou will be redirect to your dashboard'
+		  this.message = this.success
 		  setTimeout(function () {
-			window.location.href = "http://localhost:5000"; //will redirect to your blog page (an ex: blog.html)
+			window.location.href = $SCRIPT_ROOT; //will redirect to your blog page (an ex: blog.html)
 		  }, 2000);
 		} else {
-		  this.message = 'Authentification KO.\nWrong password/username'
+		  this.message = this.fail
 		} 	
 	  }, response => {
 		console.log("error");
